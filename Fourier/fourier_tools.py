59a633f4a71cd3f8d3632075308d0a40fda0f73a
fdphd15 <fpearce@gmail.com>
2018-03-06 13:28:53 -0800

    Updated to include other data analysis projects beyond Fourier

--- Fourier/fourier_tools.py
+++ Fourier/fourier_tools.py
@@ -31,6 +31,64 @@ def print_orthonormal_test(basis):
     print("If result is identity matrix, then basis vectors are ortho-normal"
     )
 
+def calc_cos(t, f, a, p):
+    """Calculate cosine function"""
+    return a * np.cos(2*np.pi*(f*t+p/360))
+
+def calc_periodic_model(t, freq, amp, pha, x0):
+    """Calculate periodic model using input "time" array, t, to recursively add
+    together cosine functions built from each sequential value in the 
+    frequency array, freq, amplitude array, amp, and phase array, pha, with
+    the last set of array values including the addition of the x-intercept, a
+    single float value, x0.
+    """
+    if len(freq) == 1 and len(amp) == 1 and len(pha) == 1:
+        return calc_cos(t, freq[0], amp[0], pha[0]) + x0
+    elif len(freq) == len(amp) and len(freq) == len(pha):
+        return calc_cos(t, freq[0], amp[0], pha[0]) + \
+                calc_periodic_model(t, freq[1:], amp[1:], pha[1:], x0)
+    else:
+        print("Error: freq, amp, and, pha arrays MUST ALL be same length")
+        print("Returning numpy array of zeros")
+        return np.zeros(t.shape)
+
+def get_ind_linspace(step, num, t0=0):
+    """Return independent variable array using np.linspace"""
+    return np.linspace(0, step*(num-1), num) + t0
+
+def get_model_ind(**parind):
+    """Generate independent variable.  Currently only a 1-D, linearly-spaced
+    array is supported via np.linspace.  Add new methods, e.g. random, etc.
+    """
+    if 'linspace' in parind:
+        return get_ind_linspace(**parind['linspace'])
+    else:
+        print("Unrecognized method for generating independent variable")
+
+def get_model_dep(t, **pardep):
+    """Generate dependent variable."""
+    if 'periodic' in pardep:
+        return calc_periodic_model(t, **pardep['periodic'])
+
+def get_input_model(**parmod):
+    """Generate model data.  Independent variable, t, is built first as it is
+    required to build the dependent variable, x.
+    """
+    model = {}
+    if 't' in parmod:
+        model['t'] = get_model_ind(**parmod['t'])
+        if 'x' in parmod:
+            model['x'] = get_model_dep(model['t'], **parmod['x'])
+        return model
+
+def get_input_data(**parinp):
+    """Load and/or generate input data for analysis."""
+    data = {}
+    data['input'] = {}
+    if 'model' in parinp:
+        data['input']['model'] = get_input_model(**parinp['model'])
+    return data
+
 def calc_orthonormal_basis(basis):
     """Returns numerically accurate orthonormal basis vectors given input
     array, basis, using the modified Gram-Schmidt process
@@ -52,165 +110,235 @@ def calc_orthonormal_basis(basis):
         otb[:, ii] /= np.linalg.norm(otb[:, ii], ord=2)
     return otb
 
-def calc_basis_periodic(ns, freq, delta, orthnorm_test=False):
+def calc_basis_periodic(freq, t, orthnorm_test=False):
     """Returns an orthonormal pair of periodic basis functions with a frequency
-    of freq in Hz, and ns samples at a sampling rate of delta, as a [ns, 2] 
-    numpy array. A test of ortho-normality is run if orthnorm_test=True, with
-    its results printed to screen.
+    of freq in Hz using the independent variable, t, a numpy array. Resulting
+    array has dimensions = [t.shape[0], 2]. The modified Gram-Schmidt process
+    is used to ensure orthonormality is accurate to within numerical precision,
+    which can be tested by setting orthnorm_test=True.
     """
-    # ToDo: Improve orthonormality using modified Gram-Schmidt process
-    t = np.linspace(0, delta*(ns-1), ns)
     basis = np.vstack((np.cos(2*np.pi*freq*t), -np.sin(2*np.pi*freq*t))).T
-    #basis /= np.linalg.norm(basis, ord=2, axis=1, keepdims=True)
     otb = calc_orthonormal_basis(basis)
     if orthnorm_test:
         print_orthonormal_test(otb)
     return otb
 
-def calc_rdft_rmsamp(inparr, freq, delta, axis=-1):
-    """Returns the amplitude and phase (in radians) of the real-valued input
-    numpy array, inparr, using a Discrete-Fourier Transform, where 
-    freq is the discrete frequency value in cycles/ax_unit, and
-    delta is the sampling interval along the axis dimension of the input array,
-    measured in the units defined along axis, ax_unit.
+def calc_rdft_ampphs(t, x, freq):
+    """Returns the amplitude and phase (in radians) of the real-valued
+    dependent variable, a numpy array, x, using a Discrete-Fourier Transform,
+    where freq is the discrete frequency value to analyze in cycles/ax_unit,
+    and t is the independent variable corresponding to x, a numpy array.
     """
-    orthnormbas = calc_basis_periodic(inparr.shape[axis], freq, delta, \
-            orthnorm_test=True
-    )
-    onb_amps = inparr.dot(orthnormbas)
-    amplitude = np.sqrt((2/ns)*sum(onb_amps**2))
-    phase = np.arctan2(onb_amps[1], onb_amps[0])
-    #print(phase*180/np.pi)
-    return (amplitude, phase)
+    # Add ability to handle multiple frequencies
+    ampphs = {}
+    orthnormbas = calc_basis_periodic(freq[0], t, orthnorm_test=True)
+    onb_amps = x.dot(orthnormbas)
+    ampphs['amp'] = np.sqrt((2/t.shape[0])*sum(onb_amps**2))
+    ampphs['pha'] = np.arctan2(onb_amps[1], onb_amps[0])
+    return ampphs
 
-def print_rdft_tests(pm_amp, pm_pha, ft_amp, ft_pha):
-    """Print tests to check the difference between the amplitude and phase
-    parameters used to build the model data, pm_amp and pm_pha, respectively,
-    and the amplitude and phase parameters calculated using the discrete
-    Fourier transform, ft_amp and ft_pha, respectively.  All input phase values
-    should be in degrees!
+def isarray_complex(xa):
+    """Returns true if input array, xa, contains at least one complex value;
+    otherwise returns false.
     """
-    print("\nDiscrete Fourier Transform test results:")
-    print("Model Input Amplitude - DFT Output Amplitude = {}".format( \
-            pm_amp-ft_amp
-    ))
-    print("Model Input Phase - DFT Output Phase = {}\n".format(pm_pha-ft_pha))
+    return any([isinstance(xv, complex) for xv in xa])
 
-# Calculate Fast-Fourier Transform (FFT) of numpy array, 
-# i.e. equally-spaced frequencies and their corresponding amplitudes
-def calc_rfft_frqamp( \
-        inparr, outkeys, rescale=True, axis=-1, delta=1, zero_pad=2
+def calc_fft_spec(x, outkeys=['freq', 'amp'], rescale=True, \
+        axis=-1, step=1, zeropad=2
 ):
-    """ Computes the Fast-Fourier transform of the real-valued input numpy
-    array, inparr, along the dimension axis. Optional input parameters:
-    delta is the sampling interval along the axis dimension of the input array,
-    zero_pad is an integer, length-multiplier that determines the number of
+    """Computes the Fast-Fourier transform of the input numpy array, x, along
+    the dimension, axis, using either rfft or fft, depending on whether the
+    input array, x, contains all real values or not, respectively.
+    Optional input parameters:
+    step is the sampling interval along the axis dimension of the input array,
+    zeropad is an integer, length-multiplier that determines the number of
     zeros appended to the end of the input numpy array prior to applying the
     FFT, such that the total number of zeros appended is equal to
-    ns*(zero_pad-1).
+    ns*(zeropad-1).
     FFT output can be returned in rescaled form (rescale=True), such that
     the amplitude values match up with the actual amplitude in the input
     array; or, the FFT output can be returned exactly as produced by numpy's
     FFT algorithm (rescale=False).
     Returns a dictionary, fftout, with each of its keys defined by the valid
-    keys provided in the input list, outkeys, and each value is the 
-    corresponding 1D numpy array with ns*zero_pad rows.
+    keys provided in the input list, outkeys, and each value is the
+    corresponding 1D numpy array with ns*zeropad rows.
     Valid strings for outkeys are as follows:
-    "frequency" for frequency vector,
-    "complex" for full, complex-valued FFT output
-    "amplitude" for amplitude spectrum,
-    "phase" for phase spectrum in radians
-    If 'complex' is in outkeys, then only 'complex is returned, while 'amplitude' 
-    and 'phase' are ignored, if present.  Otherwise, "amplitude" and/or "phase"
+    "freq" for frequency vector,
+    "comp" for full, complex-valued FFT output
+    "amp" for amplitude spectrum,
+    "pha" for phase spectrum in radians
+    If 'comp' is in outkeys, then only complex is returned, while 'amp'
+    and 'pha' are ignored, if present.  Otherwise, "amp" and/or "pha"
     are returned, if present.
     Thus, the resulting output dictionary will have at most three keys:
-        ['frequency' AND/OR ('complex' OR ('amplitude' AND/OR 'phase))']
+        ['freq' AND/OR ('comp' OR ('amp' AND/OR 'pha))']
     """
-    # ToDo: Make an option to zero pad inparr up to the nearest power of 2, 
+    # ToDo: Make an option to zero pad x up to the nearest power of 2, 
     # for speed!!!
-    ns = inparr.shape[axis]
-    nt = ns * int(zero_pad)
-    fftout = {}
-    # Compute frequency vector
-    if 'frequency' in outkeys:
-        fftout['frequency'] = np.fft.rfftfreq(nt, d=delta)
-    # Compute raw FFT output, then rescale values if rescale=True
-    rfft = np.fft.rfft(inparr, n=nt, axis=axis)
-    if rescale:
-        rfft /= ns
-        rfft[1:] *= 2
-    # Append FFT output as either complex ("raw"), amplitude, and/or phase
-    if 'complex' in outkeys:
-        # Append "raw", complex-valued output from FFT
-        fftout['complex'] = rfft
+    ns = x.shape[axis]
+    nt = ns * int(zeropad)
+    spec = {'x_complex': isarray_complex(x)}
+    if spec['x_complex']:
+        if 'freq' in outkeys:
+            spec['freq'] = np.fft.fftfreq(nt, d=step)
+        fftout = np.fft.fft(x, n=nt, axis=axis)
     else:
-        if 'amplitude' in outkeys:
-            fftout['amplitude'] = np.abs(rfft)
-        if 'phase' in outkeys:
-            fftout['phase'] = np.angle(rfft)
-    return fftout
+        if 'freq' in outkeys:
+            spec['freq'] = np.fft.rfftfreq(nt, d=step)
+        fftout = np.fft.rfft(x, n=nt, axis=axis)
+    if rescale:
+        fftout /= ns
+        fftout[1:] *= 2
+    if 'comp' in outkeys:
+        spec['comp'] = fftout
+    else:
+        if 'amp' in outkeys:
+            spec['amp'] = np.abs(fftout)
+        if 'pha' in outkeys:
+            spec['pha'] = np.angle(fftout)
+    return spec
 
-def plot_spectrum(fft_dict, savename, k1='frequency', k2='amplitude'):
+def get_analysis_fourier(datinp, **parana):
+    """Get fourier analysis data, including real discrete fourier transform
+    (rdft) and fast-fourier transform (fft).
+    """
+    ditype = parana['input']
+    datana = {}
+    if 'rdft' in parana:
+        datana['rdft'] = calc_rdft_ampphs(datinp[ditype]['t'], \
+                datinp[ditype]['x'], \
+                **parana['rdft']
+        )
+    if 'fft' in parana:
+        datana['fft'] = calc_fft_spec(datinp[ditype]['x'], \
+                **parana['fft']
+        )
+    return datana
+
+def print_rdft_tests(datdft, **parmod):
+    """Print tests to check the difference between the amplitude and phase
+    parameters used to build the model data, pm_amp and pm_pha, respectively,
+    and the amplitude and phase parameters calculated using the discrete
+    Fourier transform, datdft['amp'] and datdft['pha'], respectively.
+    All phase values should be converted to degrees!
+    """
+    pm_amp = parmod['x']['periodic']['amp'][0]
+    pm_pha = parmod['x']['periodic']['pha'][0]
+    df_pha = 180 * datdft['pha'] / np.pi
+    print("\nDiscrete Fourier Transform test results:")
+    print("Model Input Amplitude - DFT Output Amplitude = {}".format( \
+            pm_amp-datdft['amp']
+    ))
+    print("Model Input Phase - DFT Output Phase = {}\n".format(pm_pha-df_pha))
+
+def print_fft_tests(datana):
+    """Print tests to check the output of the real Fast-Fourier Transform"""
+    maxampind = data['analysis']['fft']['amp'].argmax()
+    print("Amplitude at f=0 = {}".format( \
+            data['analysis']['fft']['amp'][0]
+    ))
+    print("Frequency at max. amplitude = {} cycles/t_units".format( \
+            data['analysis']['fft']['freq'][maxampind]
+    ))
+    print("Maximum amplitude = {}".format( \
+            data['analysis']['fft']['amp'][maxampind]
+    ))
+    print("Phase at max. amplitude = {} degrees".format( \
+            180*data['analysis']['fft']['pha'][maxampind]/np.pi
+    ))
+
+def plot_spectrum(fft_dict, **parout):
     """Plot the Fourier spectrum obtained from applying the FFT to an input
-    numpy array, i.e. from the output of calc_rfft_frqamp defined above.
+    numpy array, i.e. from the output of calc_fft_spec defined above.
     """
     # Fix 'complex' key scenario to plot real or imaginary part, and change
     # label, etc.
-    ax_labels = {'amplitude': 'Amplitude', 'frequency': 'Frequency [Hz]', \
-            'phase': 'Phase [degrees]', 'complex': 'Complex Amplitude'}
-    plot_params = {'linestyle': '-', 'linewidth': 2, 'color': 'k', \
-            'marker': '*', 'markersize': 8
-    }
+    ax_labels = {'amp': 'Amplitude', 'freq': 'Frequency [Hz]', \
+            'pha': 'Phase [degrees]', 'comp': 'Complex Amplitude'}
     fig = plt.figure()
     fig.suptitle('FFT Output: {} vs {}'.format( \
-            k2.capitalize(), k1.capitalize()
+            parout['y_key'].capitalize(), parout['x_key'].capitalize()
     ))
-    ax = fig.add_subplot(111, xlabel=ax_labels[k1], ylabel=ax_labels[k2])
-    ax.plot(fft_dict[k1], fft_dict[k2], **plot_params)
-    plt.savefig(savename)
+    ax = fig.add_subplot(111, xlabel=ax_labels[parout['x_key']], \
+            ylabel=ax_labels[parout['y_key']]
+    )
+    ax.plot(fft_dict[parout['x_key']], fft_dict[parout['y_key']], \
+            **parout['params']
+    )
+    if 'save' in parout:
+        plt.savefig(parout['save']['name'])
 
+def generate_output(datana, parmod, **parout):
+    if parout['rdft_tests']:
+        print_rdft_tests(datana['rdft'], **parmod)
+    if parout['fft_tests']:
+        print_fft_tests(data['analysis']['fft'])
+    if parout['figure']['type'] == 'spectrum':
+        plot_spectrum(datana['fft'], **parout['figure'])
 
 ## III) If run from command line, execute script below here
 if __name__ == "__main__":
     # Testing parameters
-    ns = 2000
-    delta = 0.01
-    pm_freq = 2.7
-    pm_phase = 50
-    pm_amp = 8
-    pm_x0 = 5
-    fft_rescale = True
-    fft_zeropad = 1
-    fft_savename = 'test_fft.png'
+    # Model data
+    params = { \
+        'input': { \
+            'model': { \
+                't': { \
+                    'linspace': { \
+                        'num': 2000, \
+                        'step': 0.01
+                    }
+                }, \
+                'x': { \
+                    'periodic': { \
+                        'freq': [2.7, 16], \
+                        'pha': [50, 100], \
+                        'amp': [8, 4], \
+                        'x0': 5
+                    }
+                }
+            }
+        }, \
+        'analysis': { \
+            'input': 'model', \
+            'rdft': {'freq': [2.7, 16]}, \
+            'fft': { \
+                'outkeys': ['freq', 'amp', 'pha'], \
+                'step': 0.01, \
+                'rescale': 1, \
+                'zeropad': 2
+            }
+        }, \
+        'output': { \
+            'rdft_tests': 1, \
+            'fft_tests': 1, \
+            'figure': { \
+                'type': 'spectrum', \
+                'x_key': 'freq', \
+                'y_key': 'amp', \
+                'params': { \
+                    'linestyle': '-',\
+                    'linewidth': 2,\
+                    'color': 'k', \
+                    'marker': '*',\
+                    'markersize': 8
+                }, \
+                'save': { \
+                    'name': 'test_fft.png'
+                }
+            }
+        }
+    }
 
-    # Build independent, t, and dependendent, x, variables for testing
-    # assuming a periodic model that contains a single frequency component at
-    # pm_freq in cycles/t_units, with a phase angle of pm_phase in degrees, an
-    # amplitude of pm_amp, and an added constant (i.e. DC) value of pm_x0
-    t = np.linspace(0, delta*(ns-1), ns)
-    x = pm_amp * np.cos(2*np.pi*(pm_freq*t+pm_phase/360)) + pm_x0
+    # Input: build data dictionary, including independent, t, and dependendent,
+    # x, variables for testing
+    data = get_input_data(**params['input'])
 
-    # Use the Discrete Fourier Transform (DFT) to calculate the amplitude of
-    # the test data at the frequency value, pm_freq, used to produce x.
-    # This should output a value, rdft, that is roughly equal to the input 
-    # amplitude value, pm_amp, specified above
-    rdft_a, rdft_p = calc_rdft_rmsamp(x, pm_freq, delta)
-    print_rdft_tests(pm_amp, pm_phase, rdft_a, 180*rdft_p/np.pi)
+    # Analysis: calculate the Discrete Fourier Transform (DFT) and/or the
+    # Fast-Fourier Transform (FFT) to dependent variable
+    data['analysis'] = get_analysis_fourier(data['input'], **params['analysis'])
 
-    # Use the Fast-Fourier Transform (FFT) to calculate the amplitude spectrum
-    # of the test data at a discrete set of frequency values, i.e. values in
-    # fft_out['frequency']
-    fft_out = calc_rfft_frqamp(x, ['frequency', 'amplitude', 'phase'], \
-            rescale=fft_rescale, delta=delta, zero_pad=fft_zeropad
+    # Output: print results, build/save figures, etc.
+    generate_output(data['analysis'], params['input']['model'],
+            **params['output']
     )
-    maxampind = fft_out['amplitude'].argmax()
-    print("Amplitude at f=0 = {}".format(fft_out['amplitude'][0]))
-    print("Frequency at max. amplitude = {} cycles/t_units".format( \
-            fft_out['frequency'][maxampind]
-    ))
-    print("Maximum amplitude = {}".format(fft_out['amplitude'][maxampind]))
-    print("Phase at max. amplitude = {} degrees".format( \
-            180*fft_out['phase'][maxampind]/np.pi
-    ))
-    plot_spectrum(fft_out, fft_savename)
-
